4ccba4aef7019eb983afcdb39b71f03cd166c7dd
tanimorimizuki <mizuki.tanimori@rakus-partners.co.jp>
2020-02-04 17:10:46 +0900

    Controller等を修正しました。

--- src/main/java/com/example9/controller/Exam04Controller.java
+++ src/main/java/com/example9/controller/Exam04Controller.java
@@ -1,19 +1,48 @@
 package com.example9.controller;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example9.domain.User;
+import com.example9.form.UserForm;
 
 @Controller
 @RequestMapping("/exam04")
 public class Exam04Controller {
+	
+	@ModelAttribute
+	public UserForm setUpForm() {
+		return new UserForm();
+	}
 
 	@RequestMapping("")
 	public String index() {
 		return "exam04";
 	}
 	
-	@RequestMapping("/input")
-	public String inPut() {
+	@RequestMapping("/result")
+	public String result(
+			Model model
+		  , @Validated UserForm form
+		  , BindingResult result
+		  ) {
+		
+		if(result.hasErrors()) {
+			return "exam04";
+		}
+		
+		User user = new User();
+		user.setName(form.getName());
+		user.setAge(form.getAge());
+		user.setComment(form.getComment());
+		
+		model.addAttribute("user", user);
+	
 		return "exam04-result";
 	}
 }
