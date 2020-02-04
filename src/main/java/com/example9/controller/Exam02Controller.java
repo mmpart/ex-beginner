fc3aa7ff8bb3d669e5327d17369cb3a2ad213e23
tanimorimizuki <mizuki.tanimori@rakus-partners.co.jp>
2020-02-04 14:56:38 +0900

    Controller、htmlの修正を行いました。

--- src/main/java/com/example9/controller/Exam02Controller.java
+++ src/main/java/com/example9/controller/Exam02Controller.java
@@ -1,12 +1,17 @@
 package com.example9.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/exam02")
 public class Exam02Controller {
+	
+	@Autowired
+	private HttpSession session;
 
 	@RequestMapping("")
 	public String indx() {
@@ -14,12 +19,12 @@ public class Exam02Controller {
 	}
 	
 	@RequestMapping("/result")
-	public String result(Model model,String num1,String num2) {
-		model.addAttribute("num1", num1);
-		model.addAttribute("num2", num2);
+	public String result(int num1,int num2) {
+		session.setAttribute("num1", num1);
+		session.setAttribute("num2", num2);
 		
-		String answer = num1+num2;
-		model.addAttribute("answer", answer);
+		int answer = num1+num2;
+		session.setAttribute("answer", answer);
 		
 		return "exam02-result";
 	}
