f0e85954492fe2435b6cfe071f0bc8eddfab024c
tanimorimizuki <mizuki.tanimori@rakus-partners.co.jp>
2020-02-04 15:21:48 +0900

    Controller、Formを作成しました。

---
+++ src/main/java/com/example9/controller/Exam03Controller.java
@@ -0,0 +1,32 @@
+package com.example9.controller;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/exam03")
+public class Exam03Controller {
+	
+	@Autowired
+	private ServletContext application;
+
+	@RequestMapping("")
+	public String index() {
+		return "exam03";
+	}
+	
+	@RequestMapping("/result")
+	public String result(Integer money1, Integer money2, Integer money3) {
+		int tax = (int) 0.1;
+		int sum = money1+money2+money3;
+		int taxinc = sum+(sum*tax);
+		
+		application.setAttribute("sum", sum);
+		application.setAttribute("taxinc", taxinc);
+		
+		return "exam03-result";
+	}
+}
---
+++ src/main/java/com/example9/form/Exam03Form.java
@@ -0,0 +1,34 @@
+package com.example9.form;
+
+public class Exam03Form {
+
+	private Integer money1;
+	private Integer money2;
+	private Integer money3;
+	
+	public Integer getMoney1() {
+		return money1;
+	}
+	public void setMoney1(Integer money1) {
+		this.money1 = money1;
+	}
+	public Integer getMoney2() {
+		return money2;
+	}
+	public void setMoney2(Integer money2) {
+		this.money2 = money2;
+	}
+	public Integer getMoney3() {
+		return money3;
+	}
+	public void setMoney3(Integer money3) {
+		this.money3 = money3;
+	}
+	@Override
+	public String toString() {
+		return "Exam03Form [money1=" + money1 + ", money2=" + money2 + ", money3=" + money3 + "]";
+	}
+	
+	
+	
+}
